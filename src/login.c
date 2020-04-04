a7dd0955f1d9b76edb795edc9ea52ae8d201a590
Ihor Ivanov <ihor.ivanov@globallogic.com>
2020-04-04 18:24:43 +0300

    Fix segfault when LANG environment variable is not set
    Signed-off-by: Ihor Ivanov <ihor.ivanov@globallogic.com>

--- src/login.c
+++ src/login.c
@@ -227,7 +227,10 @@ void env_init(struct passwd* pwd)
 	setenv("SHELL", pwd->pw_shell, 1);
 	setenv("USER", pwd->pw_name, 1);
 	setenv("LOGNAME", pwd->pw_name, 1);
-	setenv("LANG", lang, 1);
+
+	if (lang != NULL) {
+		setenv("LANG", lang, 1);
+	}
 
 	// Set PATH if specified in the configuration
 	if (strlen(config.path))
